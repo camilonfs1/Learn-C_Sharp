41765d5f8cbb8a29d0857109a7606fdee7541799
camilonfs1 <camilonfs1@gmail.com>
2021-01-22 09:59:01 -0500

    commit

--- Hello/Hello/Program.cs
+++ Hello/Hello/Program.cs
@@ -6,7 +6,13 @@ namespace Hello
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.Clear();
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Camilo !");
+
+          
             Console.Read();
         }
     }
