e9c01dc82a6578280ba19b82627652479505336f
camilonfs1 <camilonfs1@gmail.com>
2021-01-22 10:32:26 -0500

    commit

--- First_Software/First_Software/Program.cs
+++ First_Software/First_Software/Program.cs
@@ -4,6 +4,7 @@ namespace First_Software
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             /*int num1 = 13;
@@ -24,16 +25,22 @@ namespace First_Software
 
             Console.WriteLine(mensmay + nombre); */
 
-            string miString = "15";
-            string misegundoString = "10";
+            /* string miString = "15";
+             string misegundoString = "10";
 
 
-            int num1 = Int32.Parse(miString);
-            int num2 = Int32.Parse(misegundoString);
-            int sum = num1 + num2;
+             int num1 = Int32.Parse(miString);
+             int num2 = Int32.Parse(misegundoString);
+             int sum = num1 + num2;
 
 
-            Console.WriteLine("La suma de " + num1 + " + " + num2 + " da como resultado: " + sum);
+             Console.WriteLine("La suma de " + num1 + " + " + num2 + " da como resultado: " + sum);*/
+
+            const double PI = 3.1418564359;
+            const int semanas = 52, meses = 12;
+            const string naciemiento = "10 de febrero de 1997";
+
+
             Console.Read();
         }
     }
